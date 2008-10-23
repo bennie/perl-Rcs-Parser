dc697dd6a71994ba7e8978bacc4b8ff887e4386d
bennie@cpan.org <bennie@cpan.org>
2008-10-23 18:49:46 +0000

    Cleaning up from Horus

--- lib/Rcs/Parser.pm
+++ lib/Rcs/Parser.pm
@@ -12,102 +12,17 @@ CVS and as documented by the rcsfile(5) man page.
 
 Retrieve the most recent file from an archive:
 
-  my $rcs = new RCS;
+  my $rcs = new Rcs::Parser;
   my $ret = $rcs->load($filename);
   
   my $current = $rcs->recent_version;
 
 To retrieve a specific version:
 
-  my $rcs = new RCS;
+  my $rcs = new Rcs::Parser;
   my $ret = $rcs->load($filename);
   
   my $specific_version = $rcs->get('1.2');
-
-=head1 METHODS:
-
-=head2 author($version)
-
-my $author = $rcs->author($version);
-
-The author method returns the author name of the given version. If
-no version is given it returns the author of the current loaded version.
-
-=head2 date($version)
-
-my $date = $rcs->date($version);
-
-The date method returns the revision date of the given version. If
-no version is given it returns the date of the current version.
-
-=head2 get($version)
-
-my $ret = $rcs->get($version);
-
-This method causes the given version to be retrieved from the archive
-
-=head2 load($filename)
-
-my $ret = $rcs->load($filename);
-
-The load command reads in and parses the given filename. If the
-file does not exist or is unreadable by the script, undef is 
-returned. Otherwise, 1 is returned upon success.
-
-=head2 notate()
-
-my $ret = $rcs->notate()
-
-This method builds and assembled statistical information for veriosn.
-
-=head2 previous_version()
-
-my $ver = $rcs->previous_version();
-
-This method returns the version previous to the currently instanced version.
-
-=head2 recent_version()
-
-my $ver = $rcs->recent_version();
-
-This method returns the most current revision of the file.
-
-=head2 version()
-
-my $ver = $rcs->version();
-
-This method returns the currently instanced version.
-
-=head1 KNOWN ISSUES:
-
-Beta Code:
-
-This code is alpha. It has yet to fully understand binary formats stored
-in RCS and will treat them as text. Consquently, you'll see warnings. That
-being said, there shouldn't be any large scale bugs that will cause
-segfaulting or crashing. Only warnings.
-
-The RCS file format:
-
-There is an astounding lack of good documentation of the RCS format. About
-the only thing that can be found is the rcsfile(5) man page. The layout is
-mostly reverse engineered in this module. I have yet to have the time, or
-the skill and patience to disassemble the RCS portions of the code for GNU
-CVS. Maybe soon. :)
-
-=head1 ERATTA:
-
-  Q: Why 'Rcs' and not 'RCS'
-
-  A: Because the any directory named 'RCS' is usually ignored by most 
-     versioning software and some developer tools. 
-
-=head1 AUTHORSHIP:
-
-    Rcs::Parser v0.02 2004/04/02
-
-    (c) 2001-2004, Phillip Pollard <bennie@cpan.org>
-    Released under the Perl Artistic License
 
 =cut
 
@@ -124,15 +39,26 @@ CVS. Maybe soon. :)
 #*************************************************************************
 
 package Rcs::Parser;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use 5.006;
-#use warnings; Soon!
+use warnings;
 use strict;
 
 sub new {
   bless {}, $_[0]
 }
+
+=head1 METHODS:
+
+=head2 author($version)
+
+my $author = $rcs->author($version);
+
+The author method returns the author name of the given version. If
+no version is given it returns the author of the current loaded version.
+
+=cut 
 
 sub author {
   my $self = shift @_;
@@ -140,11 +66,28 @@ sub author {
   return $self->{rcs}->{$ver}->{author} || undef;
 }
 
+=head2 date($version)
+
+my $date = $rcs->date($version);
+
+The date method returns the revision date of the given version. If
+no version is given it returns the date of the current version.
+
+=cut
+
 sub date {
   my $self = shift @_;
   my $ver  = shift @_ || $self->recent_version;
   return $self->{rcs}->{$ver}->{date} || undef;
 }
+
+=head2 get($version)
+
+my $ret = $rcs->get($version);
+
+This method causes the given version to be retrieved from the archive
+
+=cut
 
 sub get {
   my $self = shift @_;
@@ -163,6 +106,16 @@ sub get {
   return $self->_dump;
 }
 
+=head2 load($filename)
+
+my $ret = $rcs->load($filename);
+
+The load command reads in and parses the given filename. If the
+file does not exist or is unreadable by the script, undef is 
+returned. Otherwise, 1 is returned upon success.
+
+=cut
+
 sub load {
   my $self      = shift @_;
   $self->{file} = shift @_;
@@ -171,7 +124,12 @@ sub load {
   my $doc_header;
   $self->{rcs} = {};
 
-  $self->_parse_in_rcs($self->{file},$self->{rcs});
+  open RCSFILE, '<', $self->{file};
+  my @raw = <RCSFILE>;
+  close RCSFILE;
+
+  $self->{rawfile} = \@raw; # The rawfile is steadily deleted as it is parsed
+  $self->_parse_in_rcs($self->{rcs});
 
   # populate the current doc
   
@@ -188,20 +146,49 @@ sub load {
   return 1;
 }
 
-sub _grab_note {
-  my $self = shift @_;
-  my $ref  = shift @_;  
-  my $ver = $self->{current_document}->{version};
-  for my $line ( keys %{ $self->{current_document}->{body} } ) {
-    $ref->{$ver}->{body}->{$line}->{line}   = length $self->{current_document}->{body}->{$line}->{line};
-    $ref->{$ver}->{body}->{$line}->{origin} = $self->{current_document}->{body}->{$line}->{origin};  
+=head2 load_scalar($data)
+
+my $ret = $rcs->load_scalar($data);
+
+The load command reads in and parses the provided RCS file in scalar 
+form. 1 is returned upon success.
+
+=cut
+
+sub load_scalar {
+  my $self  = shift @_;
+  my $data  = shift @_;
+
+  my $doc_header;
+  $self->{rcs} = {};
+
+  my @raw = map { "$_\n" } split "\n", $data;
+
+  $self->{rawfile} = \@raw; # The rawfile is steadily deleted as it is parsed
+  $self->_parse_in_rcs($self->{rcs});
+
+  # populate the current doc
+  
+  $self->{current_document}->{version} = $self->recent_version;
+
+  for my $line ( split /\n/, $self->{rcs}->{$self->recent_version}->{text} ) {
+    push @{ $self->{current_document}->{body}->{1}->{new_lines} }, $self->_unquote($line) . "\n";
   }
-  for my $map ( keys %{ $self->{current_document}->{line_map} } ) {
-    $ref->{$ver}->{line_map}->{$map} = $self->{current_document}->{line_map}->{$map};
-  }
+
+  # resort it
+
+  $self->_sort;
 
   return 1;
 }
+
+=head2 notate()
+
+my $ret = $rcs->notate()
+
+This method builds and assembled statistical information for verions.
+
+=cut
 
 sub notate {
   my $self = shift @_;
@@ -254,23 +241,79 @@ sub notate {
   return $note;
 }
 
+sub _grab_note {
+  my $self = shift @_;
+  my $ref  = shift @_;  
+  my $ver = $self->{current_document}->{version};
+  for my $line ( keys %{ $self->{current_document}->{body} } ) {
+    $ref->{$ver}->{body}->{$line}->{line}   = length $self->{current_document}->{body}->{$line}->{line};
+    $ref->{$ver}->{body}->{$line}->{origin} = $self->{current_document}->{body}->{$line}->{origin};  
+  }
+  for my $map ( keys %{ $self->{current_document}->{line_map} } ) {
+    $ref->{$ver}->{line_map}->{$map} = $self->{current_document}->{line_map}->{$map};
+  }
+
+  return 1;
+}
+
+=head2 all_versions()
+
+my @versions = $rcs->all_versions();
+
+This method returns an array or arrayref of all versions stored in the RCS file.
+
+=cut
+
+sub all_versions {
+  my $self = shift @_;
+  our @versions;
+  unless ( defined @versions ) {
+    @versions = sort { my @a = split '\.', $a; my @b = split '\.', $b; $b[0] <=> $a[0] || $b[1] <=> $a[1] } grep !/header/, keys %{$self->{rcs}};
+  }
+  return wantarray ? @versions : \@versions;
+}
+
+=head2 previous_version()
+
+my $ver = $rcs->previous_version();
+
+This method returns the version previous to the currently instanced version.
+
+=cut
+
 sub previous_version {
   my $self = shift @_;
   my $ver  = shift @_ || $self->recent_version;
   return $self->{rcs}->{revision_path}->{$ver};
 }
 
+=head2 recent_version()
+
+my $ver = $rcs->recent_version();
+
+This method returns the most current revision of the file.
+
+=cut
+
 sub recent_version {
   my $self = shift @_;
   return $self->{rcs}->{header}->{head};
 }
+
+=head2 version()
+
+my $ver = $rcs->version();
+
+This method returns the currently instanced version.
+
+=cut
 
 sub version {
   my $self = shift @_;
   return $self->{current_document}->{version};
 }
 
-####
+#### Private methods
 
 sub _apply_delta {
   my $self = shift @_;
@@ -344,16 +387,13 @@ sub _dump {
 
 sub _parse_in_rcs {
   my $self = shift @_;
-  my $file = shift @_;
   my $rcs  = shift @_;
- 
-  open RCSFILE, $file;
 
   ### Parse in the RCS file header
 
   my $rcs_header;
 
-  while ( my $line = <RCSFILE> ) {
+  while ( my $line = shift @{$self->{rawfile}} ) {
     last if $line =~ /^$/;
     $rcs_header .= $line;
   }
@@ -369,7 +409,7 @@ sub _parse_in_rcs {
   ### Parse in the individual version headers
 
   my $version = 'error';
-  while ( my $line = <RCSFILE> ) {
+  while ( my $line = shift @{$self->{rawfile}} ) {
     last if $line =~ /^desc$/;
 
     if ( $line =~ /^([\d|\.]+)/ ) {
@@ -380,7 +420,7 @@ sub _parse_in_rcs {
     }
   }
 
-  my $garbage_double_quote = <RCSFILE>;
+  my $garbage_double_quote = shift @{$self->{rawfile}};
 
   ### Parse in the individual deltas
 
@@ -388,7 +428,7 @@ sub _parse_in_rcs {
   my $directive = 0;
   my $quote = 0;
 
-  while ( my $line = <RCSFILE> ) {
+  while ( my $line = shift @{$self->{rawfile}} ) {
     $version = $1 if $line =~ /^([\d|\.]+)/ && $quote == 0;
     if ( $line =~ /^\@(?!\@)/ ) { $quote = $quote == 0 ? 1 : 0; }
     next if $line =~ /^$/ && $quote == 0;
@@ -401,9 +441,8 @@ sub _parse_in_rcs {
     }
 
     #$rcs{$version}{raw_text} .= $line;
+    $self->_debug("Line for delta in $version : $line");
   }
-  
-  close RCSFILE;
 
   ### clean the leftover quoting
 
@@ -417,7 +456,8 @@ sub _parse_in_rcs {
   ### disassemble header
 
   for my $version ( keys %$rcs ) {
-    my @commands = split ';', $rcs->{$version}->{header};
+    my @commands;
+    @commands = split ';', $rcs->{$version}->{header} if defined $rcs->{$version}->{header};
     for my $command ( @commands ) {
       chomp $command;
       $rcs->{$version}->{$1} = $2 if $command =~ /\s*(.+)\s+(.+)$/;
@@ -488,3 +528,34 @@ sub _unquote {
 }
 
 1;
+
+=head1 KNOWN ISSUES:
+
+Beta Code:
+
+This code is beta. It has yet to fully understand binary formats stored
+in RCS and will treat them as text. Consquently, you'll see warnings. That
+being said, there shouldn't be any large scale bugs that will cause
+segfaulting or crashing. Only warnings.
+
+The RCS file format:
+
+There is an astounding lack of good documentation of the RCS format. About
+the only thing that can be found is the rcsfile(5) man page. The layout is
+mostly reverse engineered in this module. I have yet to have the time, or
+the skill and patience to disassemble the RCS portions of the code for GNU
+CVS.
+
+=head1 ERATTA:
+
+  Q: Why 'Rcs' and not 'RCS'
+
+  A: Because the any directory named 'RCS' is usually ignored by most 
+     versioning software and some developer tools. 
+
+=head1 AUTHORSHIP:
+
+    Rcs::Parser v0.02 2004/04/02
+
+    (c) 2001-2008, Phillip Pollard <bennie@cpan.org>
+    Released under the Perl Artistic License
